83c646d9658a12caf7551429f8ca4de3e987e616
Marcilio Júnior <jrsmarcilio@gmail.com>
2023-05-02 19:15:14 -0300

    fix: remove dist

--- dist/config/connection.d.ts
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import Stomp from 'stompjs';
-declare const client: Stomp.Client;
-export default client;
--- dist/config/stomp/connection.d.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import Stomp, { Frame } from "stompjs";
-declare const client: Stomp.Client;
-type ConnectCallback = (frame?: Frame) => any;
-type ErrorCallback = (error: Frame | string) => any;
-export declare const stompConnection: (connectCallback: ConnectCallback, errorCallback?: ErrorCallback) => any;
-export default client;
